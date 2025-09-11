fe46a7fcddc6bd2f1db2ab38719832dc749be6ed
thefonso <unclefonso@protonmail.com>
2025-09-11 17:32:32 -0400

    basic pages linking and omponent sharing

--- web/src/components/ArticlesCell/ArticlesCell.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.tsx
@@ -1,16 +1,21 @@
 import type { ArticlesQuery, ArticlesQueryVariables } from 'types/graphql'
 
 import type {
-  CellSuccessProps,
-  CellFailureProps,
-  TypedDocumentNode,
+        CellSuccessProps,
+        CellFailureProps,
+        TypedDocumentNode,
 } from '@redwoodjs/web'
 
+import Article from 'src/components/Article'
+
 export const QUERY: TypedDocumentNode<ArticlesQuery, ArticlesQueryVariables> =
-  gql`
+        gql`
     query ArticlesQuery {
-      articles {
+       articles: posts {
         id
+        title
+        body
+        createdAt
       }
     }
   `
@@ -20,19 +25,17 @@ export const Loading = () => <div>Loading...</div>
 export const Empty = () => <div>Empty</div>
 
 export const Failure = ({
-  error,
+        error,
 }: CellFailureProps<ArticlesQueryVariables>) => (
-  <div style={{ color: 'red' }}>Error: {error?.message}</div>
+        <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
-export const Success = ({
-  articles,
-}: CellSuccessProps<ArticlesQuery, ArticlesQueryVariables>) => {
-  return (
-    <ul>
-      {articles.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
-  )
+export const Success = ({ articles, }: CellSuccessProps<ArticlesQuery, ArticlesQueryVariables>) => {
+        return (
+                <>
+                        {articles.map((article) => (
+                                <Article key={article.id} article={article} />
+                        ))}
+                </>
+        )
 }
--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,11 @@
 import { Metadata } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
         return (
                 <>
                         <Metadata title="Home" description="Home page" />
+                        <><ArticlesCell /></>
                 </>
         )
 }
