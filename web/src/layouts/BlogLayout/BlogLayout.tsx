9429cac0f38e7b07e9cd6f1c755b96a8dc9c51b2
thefonso <unclefonso@protonmail.com>
2025-09-15 22:49:30 -0400

    basic home rolled user authentication

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -1,30 +1,46 @@
 import { Link, routes } from '@redwoodjs/router'
 
+import { useAuth } from 'src/auth'
+
 type BlogLayoutProps = {
-  children?: React.ReactNode
+        children?: React.ReactNode
 }
 
 const BlogLayout = ({ children }: BlogLayoutProps) => {
-  return (<>
-    <header>
-      <h1><Link to={routes.home()}> Unclefonso Blog</Link></h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to={routes.home()}>Home</Link>
-          </li>
-          <li>
-            <Link to={routes.about()}>About</Link>
-          </li>
-          <li>
-            <Link to={routes.contact()}>contact</Link>
-          </li>
+        const { isAuthenticated, currentUser, logOut } = useAuth()
 
-        </ul>
-      </nav>
-    </header>
-    <main>{children}</main>
-  </>)
+        return (<>
+                <header>
+                        <div className="flex-between">
+                                <h1><Link to={routes.home()}> Unclefonso Blog</Link></h1>
+                                {isAuthenticated ? (
+                                        <div>
+                                                <span>Logged in as {currentUser.email}</span>{' '}
+                                                <button type="button" onClick={logOut}>
+                                                        Logout
+                                                </button>
+                                        </div>
+                                ) : (
+                                        <Link to={routes.login()}>Login</Link>
+                                )}
+                        </div>
+                        <nav>
+                                <ul>
+                                        <li>
+                                                <Link to={routes.home()}>Home</Link>
+                                        </li>
+                                        <li>
+                                                <Link to={routes.about()}>About</Link>
+                                        </li>
+                                        <li>
+                                                <Link to={routes.contact()}>contact</Link>
+                                        </li>
+
+                                </ul>
+                        </nav>
+                </header>
+                <main>{children}</main>
+        </>)
 }
 
 export default BlogLayout
