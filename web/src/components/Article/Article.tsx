ea3e9dcd3b0e05861d5556946c3d2894e402cfe7
thefonso <unclefonso@protonmail.com>
2025-09-20 00:47:06 -0400

    storybook added

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -2,19 +2,19 @@ import { Link, routes } from '@redwoodjs/router'
 import type { Post } from 'types/graphql'
 
 interface Props {
-        article: Post
+  article: Post
 }
 
 const Article = ({ article }: Props) => {
-        return (
-                <article>
-                        <header>
-                                <h2><Link to={routes.article({ id: article.id })}>{article.title}</Link></h2>
-                        </header>
-                        <div>{article.body}</div>
-                        <div>Posted at: {article.createdAt}</div>
-                </article>
-        )
+  return (
+    <article>
+      <header>
+        <h2><Link to={routes.article({ id: article.id })}>{article.title}</Link></h2>
+      </header>
+      <div>{article.body}</div>
+      <div>Posted at: {article.createdAt}</div>
+    </article>
+  )
 }
 
 export default Article
