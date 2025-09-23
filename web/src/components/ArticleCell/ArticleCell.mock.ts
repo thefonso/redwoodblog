f82e990218f6e1b6e18ce044a9914a6e523fbf75
thefonso <unclefonso@protonmail.com>
2025-09-22 23:51:35 -0400

    intro to testing begins

--- web/src/components/ArticleCell/ArticleCell.mock.ts
+++ web/src/components/ArticleCell/ArticleCell.mock.ts
@@ -1,7 +1,11 @@
 // Define your own mock data here:
 export const standard = (/* vars, { ctx, req } */) => ({
-  article: {
-    __typename: 'Article' as const,
-    id: 42,
-  },
+        article: {
+                __typename: 'Article' as const,
+                id: 42,
+                title: 'First Post',
+                body: `Look, let's start with some tough love, alright? Ready for this? Here it goes: you two suck at peddling meth. Period. Good! 'Oh boo-hoo, I won't cook meth anymore!' You're a crybaby! Who needs you?! Hey, I'm unplugging the website, so no more money laundering! How do you like that?!`,
+                createdAt: '2025-01-01T12:34:56Z',
+
+        },
 })
